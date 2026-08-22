bdc54f276d0af176b97f987ad54a583cd0be2c9a
CHRISTOPHER OKOH <etzkristokency2@gmail.com>
2026-08-22 02:00:29 +0000

    day1 variable and values

---
+++ day1_variable_and_value/exercise1.py
@@ -0,0 +1,36 @@
+name = "Christopher"
+print(name)
+
+age = 26
+height = 42.2
+student_status = "male"
+age = 20
+
+print(age)
+print(height)
+print(student_status)
+
+num1 = 10
+num2 = 20
+print(num1 + num2)
+
+##Micro Project 
+##1
+name = "Christopher"
+age = 12
+is_student = True
+
+print(name)
+print(age)
+print(is_student)
+
+##2
+price = 30
+quantity = 40
+print(price + quantity)
+
+##3
+city = "Abuja"
+print(city)
+city = "lagos"
+print(city)
