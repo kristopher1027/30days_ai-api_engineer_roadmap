ebf29dea2bf2889a6b746e1da108fc6156589efe
CHRISTOPHER OKOH <etzkristokency2@gmail.com>
2026-09-01 13:14:30 +0100

    Add variable initialization and type checking
    Initialize variables and demonstrate type conversion.

---
+++ day2_data_type_and_type_convertion/main.py
@@ -0,0 +1,16 @@
+name = "christopher"
+age = 25
+average = 3.5
+status = True
+
+print(type(name))
+print(type(age))
+print(type(average))
+print(type(status))
+
+age = "25"
+print(type(age))
+age = int(age)
+print(age)
+print(type(age))
+
